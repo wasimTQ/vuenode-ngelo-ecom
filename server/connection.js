828f726da486f19235baa8d98cb27036939d41d0
wasimTQ <wasimthoufiq21@gmail.com>
2021-01-10 11:09:01 +0530

    deleted postgres UIL

--- server/connection.js
+++ server/connection.js
@@ -8,15 +8,5 @@
     }
   });
 
-  // const hostname = "postgres://dqwhhqehhqicdy:5843c101c40a02e78fba6034fafb6108dc0bcd3172820e34eb70dcd87e89c214@ec2-52-202-146-43.compute-1.amazonaws.com:5432/d1ucdjnmn9u53s"
-
-  // const db = new Sequelize(hostname, {
-  //   dialect:  'postgres',
-  //   protocol: 'postgres',
-  //   logging:  true,
-  //   define: {
-  //     timestamps: false
-  //   }
-  // })
 
   module.exports = db;
